3d25e5bda53077cb6bc98097588017b4d88337fe
Antoine Chassagne <antoine.chassagne@me.com>
2021-06-17 15:20:15 +0200

    Fix assets on build

--- src/build.js
+++ src/build.js
@@ -1,19 +1,24 @@
-const fs = require("fs");
 const fse = require("fs-extra");
 const path = require("path");
 
 /* Create the build directory */
 const buildPath = path.join(__dirname, "../build");
 
-if (!fs.existsSync(buildPath)) {
-  fs.mkdirSync(buildPath);
+if (!fse.existsSync(buildPath)) {
+  fse.mkdirSync(buildPath);
 }
 
 /* Copy assets directory to build directory */
 const assetsDirectoryPath = path.join(__dirname, "assets");
+const buildAssetsDirectoryPath = path.join(__dirname, "../build/assets");
 
-fse.copySync(assetsDirectoryPath, buildPath, { override: true }, (err) => {
-  if (err) {
-    console.error("Error while copying assets directory on build");
+fse.copySync(
+  assetsDirectoryPath,
+  buildAssetsDirectoryPath,
+  { override: true },
+  (err) => {
+    if (err) {
+      console.error("Error while copying assets directory on build");
+    }
   }
-});
+);
